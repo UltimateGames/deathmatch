ab883ad9fb20186f359eba589e20130962826e53
Austin Payne <payneaustin5@gmail.com>
2013-08-05 22:39:20 -0500

    Updated to latest UG API

--- src/main/java/com/greatmancode/deathmatch/Deathmatch.java
+++ src/main/java/com/greatmancode/deathmatch/Deathmatch.java
@@ -21,11 +21,10 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
@@ -108,7 +107,7 @@ public class Deathmatch extends GamePlugin {
 			}
 		}
 		ultimateGames.getScoreboardManager().removeArenaScoreboard(arena, "Kills");
-		ultimateGames.getMessageManager().broadcastReplacedGameMessageToArena(game, arena, "GameEnd", highestScorer, game.getGameDescription().getName(), arena.getName());
+		ultimateGames.getMessageManager().broadcastReplacedGameMessage(game, "GameEnd", highestScorer, game.getGameDescription().getName(), arena.getName());
 		for (String playerName : players) {
 			ultimateGames.getPlayerManager().removePlayerFromArena(playerName, arena, false);
 		}
@@ -164,34 +163,42 @@ public class Deathmatch extends GamePlugin {
 		
 	}
 	
-	
-
-	@SuppressWarnings("deprecation")
-	private void resetInventory(Player player) {
-		player.getInventory().clear();
-		player.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1), new ItemStack(Material.BOW, 1), new ItemStack(Material.ARROW, 32));
-		String playerName = player.getName();
-		if (ultimateGames.getPlayerManager().isPlayerInArena(playerName)) {
-			Arena arena = ultimateGames.getPlayerManager().getPlayerArena(playerName);
-			player.getInventory().addItem(ultimateGames.getUtils().createInstructionBook(arena.getGame()));
-		}
-		player.getInventory().setArmorContents(new ItemStack[] {new ItemStack(Material.LEATHER_BOOTS, 1), new ItemStack(Material.LEATHER_LEGGINGS, 1), new ItemStack(Material.LEATHER_CHESTPLATE, 1), new ItemStack(Material.LEATHER_HELMET, 1)});
-		player.updateInventory();
-	}
-
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerDamageByPlayer(EntityDamageByEntityEvent event) {
-		if (!(event.getEntity() instanceof Player)) {
-			return;
-		}
-		Player damaged = (Player) event.getEntity();
-		Entity damager = event.getDamager();
-		
-		if (ultimateGames.getPlayerManager().isPlayerInArena(damaged.getName())) {
-			Arena arena = ultimateGames.getPlayerManager().getPlayerArena(damaged.getName());
-			if (!arena.getGame().equals(game)) {
-				return;
+	@Override
+	public void onPlayerDeath(Arena arena, PlayerDeathEvent event) {
+		String killerName = null;
+		Player killer = event.getEntity().getKiller();
+		if (killer != null) {
+			killerName = killer.getName();
+			if (ultimateGames.getPlayerManager().isPlayerInArena(killer.getName()) && ultimateGames.getPlayerManager().getPlayerArena(killer.getName()).equals(arena)) {
+				killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20*2, 5));
 			}
+		}
+		for (ArenaScoreboard scoreBoard : ultimateGames.getScoreboardManager().getArenaScoreboards(arena)) {
+			if (scoreBoard.getName().equals("Kills") && killerName != null) {
+				scoreBoard.setScore(killerName, scoreBoard.getScore(killerName) + 1);
+			}
+		}
+		event.getDrops().clear();
+		ultimateGames.getUtils().autoRespawn(event.getEntity());
+	}
+	
+	@Override
+	public void onPlayerRespawn(Arena arena, PlayerRespawnEvent event) {
+		event.setRespawnLocation(ultimateGames.getSpawnpointManager().getRandomSpawnPoint(arena).getLocation());
+		resetInventory(event.getPlayer());
+	}
+	
+	@Override
+	public void onEntityDamage(Arena arena, EntityDamageEvent event) {
+		if (event.getEntity() instanceof Player) {
+			event.setCancelled(true);
+		}
+	}
+	
+	@Override
+	public void onEntityDamageByEntity(Arena arena, EntityDamageByEntityEvent event) {
+		if (event.getEntity() instanceof Player) {
+			Entity damager = event.getDamager();
 			if (damager instanceof Arrow) {
 				LivingEntity entity = ((Arrow) damager).getShooter();
 				if (entity instanceof Player) {
@@ -204,53 +211,21 @@ public class Deathmatch extends GamePlugin {
 		}
 	}
 	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerDamage(EntityDamageEvent event) {
-		if (event.getEntity() instanceof Player) {
-			String playerName = ((Player) event.getEntity()).getName();
-			if (ultimateGames.getPlayerManager().isPlayerInArena(playerName) && ultimateGames.getPlayerManager().getPlayerArena(playerName).getStatus() != ArenaStatus.RUNNING) {
-				event.setCancelled(true);
-			}
-		}
+	@Override
+	public void onPlayerInteract(Arena arena, PlayerInteractEvent event) {
+		
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerDeath(PlayerDeathEvent event) {
-		Player player = (Player) event.getEntity();
+	@SuppressWarnings("deprecation")
+	private void resetInventory(Player player) {
+		player.getInventory().clear();
+		player.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1), new ItemStack(Material.BOW, 1), new ItemStack(Material.ARROW, 32));
 		String playerName = player.getName();
 		if (ultimateGames.getPlayerManager().isPlayerInArena(playerName)) {
 			Arena arena = ultimateGames.getPlayerManager().getPlayerArena(playerName);
-			if (!arena.getGame().equals(game)) {
-				return;
-			}
-
-			String killerName = null;
-			Player killer = event.getEntity().getKiller();
-			if (killer != null) {
-				killerName = killer.getName();
-				if (ultimateGames.getPlayerManager().isPlayerInArena(killer.getName()) && ultimateGames.getPlayerManager().getPlayerArena(killer.getName()).equals(arena)) {
-					killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20*10, 2));
-				}
-			}
-			for (ArenaScoreboard scoreBoard : ultimateGames.getScoreboardManager().getArenaScoreboards(arena)) {
-				if (scoreBoard.getName().equals("Kills") && killerName != null) {
-					scoreBoard.setScore(killerName, scoreBoard.getScore(killerName) + 1);
-				}
-			}
-			event.getDrops().clear();
-			ultimateGames.getUtils().autoRespawn(player);
+			player.getInventory().addItem(ultimateGames.getUtils().createInstructionBook(arena.getGame()));
 		}
-	}
-
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerRespawn(PlayerRespawnEvent event) {
-		if (ultimateGames.getPlayerManager().isPlayerInArena(event.getPlayer().getName())) {
-			Arena arena = ultimateGames.getPlayerManager().getPlayerArena(event.getPlayer().getName());
-			if (!arena.getGame().equals(game)) {
-				return;
-			}
-			event.setRespawnLocation(ultimateGames.getSpawnpointManager().getRandomSpawnPoint(arena).getLocation());
-			resetInventory(event.getPlayer());
-		}
+		player.getInventory().setArmorContents(new ItemStack[] {new ItemStack(Material.LEATHER_BOOTS, 1), new ItemStack(Material.LEATHER_LEGGINGS, 1), new ItemStack(Material.LEATHER_CHESTPLATE, 1), new ItemStack(Material.LEATHER_HELMET, 1)});
+		player.updateInventory();
 	}
 }
